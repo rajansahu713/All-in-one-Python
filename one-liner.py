624cbe5d09c26e20be614f0964ba64b998073fda
Rajan <rajansahu713@gmail.com>
2021-12-10 23:20:00 +0530

    one liner

---
+++ one-liner.py
@@ -0,0 +1,12 @@
+# one liner code for half pyramid pattern
+print('\n'.join('* ' * i for i in range(1, 5 + 1)))
+
+#list of even numbers in a list
+print([i for i in range(1, 11) if i % 2 == 0])
+
+
+# Swap Two Variables Python One-Liner
+a=5
+b=10
+a, b = b, a
+print(a, b)
