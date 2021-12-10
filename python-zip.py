a09aaa27f7fb3dc4c8b480c663b8b89265064c14
Rajan <rajansahu713@gmail.com>
2021-12-10 09:11:40 +0530

    zip

---
+++ python-zip.py
@@ -0,0 +1,13 @@
+
+
+player = ['Rohit', 'Dhoni', 'Kohli']
+odi_centuries = [29, 10, 43]
+
+records = zip(player, odi_centuries)
+
+print(list(records))
+print(list(zip(player)))
+print(dict(zip(player, odi_centuries)))
+
+
+
